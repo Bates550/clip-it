76cf1b49f5eab25e171136b8cbf94077e33d45ab
Sean Spearman <smspearmandev@gmail.com>
2020-04-20 23:39:16 -0700

    Save values from browserAction in localStorage

--- background_script.js
+++ background_script.js
@@ -1,24 +1,23 @@
 // Put all the javascript code here, that you want to execute in background.
-const state = {
-  query: "",
-  variables: {},
-};
+// const state = {
+//   query: "",
+//   variables: {},
+// };
 
-function update(message) {
-  switch (message.type) {
-    case "update_query":
-      state.query = message.payload;
-      console.log(state);
-      break;
-    default:
-      console.error(
-        "Unrecognized message.type: ",
-        message.type,
-        "from message: ",
-        message
-      );
-      break;
-  }
-}
+// function update(message) {
+//   switch (message.type) {
+//     case "update_format":
+//       console.log(localStorage.getItem("format"));
+//       break;
+//     default:
+//       console.error(
+//         "Unrecognized message.type: ",
+//         message.type,
+//         "from message: ",
+//         message
+//       );
+//       break;
+//   }
+// }
 
-browser.runtime.onMessage.addListener(update);
+// browser.runtime.onMessage.addListener(update);
--- browserAction/script.js
+++ browserAction/script.js
@@ -1,6 +1,31 @@
-document.getElementById("format").addEventListener("blur", (e) => {
-  browser.runtime.sendMessage({
-    type: "update_query",
-    payload: e.target.value,
-  });
+formatEl = document.getElementById("format");
+variableName0El = document.getElementById("variableName0");
+variableQuery0El = document.getElementById("variableQuery0");
+
+formatEl.addEventListener("blur", (e) => {
+  localStorage.setItem("format", e.target.value);
 });
+
+variableName0El.addEventListener("blur", (e) => {
+  localStorage.setItem("variableName0", e.target.value);
+});
+
+variableQuery0El.addEventListener("blur", (e) => {
+  localStorage.setItem("variableQuery0", e.target.value);
+});
+
+/*** HYDRATE ***/
+const formatValue = localStorage.getItem("format");
+if (formatValue) {
+  formatEl.value = formatValue;
+}
+
+const variableName0Value = localStorage.getItem("variableName0");
+if (variableName0Value) {
+  variableName0El.value = variableName0Value;
+}
+
+const variableQuery0Value = localStorage.getItem("variableQuery0");
+if (variableQuery0Value) {
+  variableQuery0El.value = variableQuery0Value;
+}
