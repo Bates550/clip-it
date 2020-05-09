d14bad93c612742a5bc9f7c1e9fe0af64d66ec70
Sean Spearman <smspearmandev@gmail.com>
2020-05-09 13:28:33 -0700

    Make Add variable button add fields for variable name and query

--- browserAction/script.js
+++ browserAction/script.js
@@ -2,6 +2,7 @@ formatEl = document.getElementById("format");
 variableName0El = document.getElementById("variableName0");
 variableQuery0El = document.getElementById("variableQuery0");
 clipItEl = document.getElementById("clipIt");
+addButtonEl = document.getElementById("addButton");
 
 formatEl.addEventListener("blur", (e) => {
   localStorage.setItem("format", e.target.value);
@@ -22,6 +23,29 @@ clipItEl.addEventListener("click", (e) => {
       "variableQuery0"
     )})`,
   });
+});
+
+addButtonEl.addEventListener("click", () => {
+  const labelEl = document.createElement("label");
+  const labelText = document.createTextNode("Variable Name");
+  const input = document.createElement("input");
+  input.setAttribute("id", "myId");
+
+  labelEl.appendChild(labelText);
+  labelEl.appendChild(input);
+
+  const variablesContainerEl = document.getElementById("variablesContainer");
+  variablesContainerEl.appendChild(labelEl);
+
+  const labelEl2 = document.createElement("label");
+  const labelText2 = document.createTextNode("Variable Query");
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("id", "myId2");
+
+  labelEl2.appendChild(labelText2);
+  labelEl2.appendChild(textarea);
+
+  variablesContainerEl.appendChild(labelEl2);
 });
 
 /*** HYDRATE ***/
