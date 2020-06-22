87f4b1c619d46b6d15d7663f2ef51031de4e5238
Sean Spearman <smspearmandev@gmail.com>
2020-06-21 20:03:28 -0700

    Migrate clipit button to vue

--- background_script.js
+++ background_script.js
@@ -1,7 +1,7 @@
 // Put all the javascript code here, that you want to execute in background.
 
 function update(message) {
-  const { format } = JSON.parse(localStorage.getItem("state"));
+  const format = JSON.parse(localStorage.getItem("format"));
 
   const result = message.reduce((acc, query) => {
     return acc.replace(`%${query.name}`, query.result);
--- browserAction/script.js
+++ browserAction/script.js
@@ -1,11 +1,3 @@
-// formatEl = document.getElementById("format");
-clipItEl = document.getElementById("clipIt");
-addButtonEl = document.getElementById("addButton");
-
-// formatEl.addEventListener("blur", (e) => {
-//   StateHandler.setFormat(e.target.value);
-// });
-
 new Vue({
   el: "#app",
   data: {
@@ -20,6 +12,23 @@ new Vue({
   methods: {
     addVariable: function () {
       this.variables.push({ name: "", query: "" });
+    },
+    clipIt: function () {
+      const variablesString = JSON.stringify(this.variables);
+      console.log(variablesString);
+      browser.tabs.executeScript({
+        code: `(function() {
+      let variables = ${variablesString};
+      let queryResults = variables.map((variable) => {
+        return { 
+          result: eval(variable.query),
+          name: variable.name
+        };
+      });
+
+      browser.runtime.sendMessage(queryResults);
+    })()`,
+      });
     },
   },
   watch: {
@@ -44,113 +53,3 @@ new Vue({
     }
   },
 });
-
-clipItEl.addEventListener("click", (e) => {
-  const { variables } = JSON.parse(localStorage.getItem("state"));
-  const variablesString = JSON.stringify(variables);
-  browser.tabs.executeScript({
-    code: `
-      let variables = ${variablesString};
-      let queryResults = variables.map((variable) => {
-        return { 
-          result: eval(variable.query),
-          name: variable.name
-        };
-      });
-
-      browser.runtime.sendMessage(queryResults);
-    `,
-  });
-});
-
-addButtonEl.addEventListener("click", () => {
-  StateHandler.addVariable();
-});
-
-const createVariableHTML = (id, variable) => {
-  const variablesContainerEl = document.getElementById("variablesContainer");
-
-  const section = document.createElement("section");
-  section.setAttribute("style", "display: flex; flex-direction: column");
-  section.setAttribute("id", id);
-
-  const variableNameId = `variableName_${id}`;
-  const variableQueryId = `variableQuery_${id}`;
-  section.innerHTML = `<label>Variable Name<input id="${variableNameId}"/></label><label>Variable Query<textarea id="${variableQueryId}"></textarea></label>`;
-
-  variablesContainerEl.appendChild(section);
-
-  const variableNameInput = document.getElementById(variableNameId);
-  variableNameInput.value = variable.name;
-  variableNameInput.addEventListener("blur", (e) => {
-    StateHandler.setName(id, e.target.value);
-  });
-
-  const variableQueryInput = document.getElementById(variableQueryId);
-  variableQueryInput.value = variable.query;
-  variableQueryInput.addEventListener("blur", (e) => {
-    StateHandler.setQuery(id, e.target.value);
-  });
-};
-
-/*** HYDRATE ***/
-const variableName0Value = localStorage.getItem("variableName0");
-if (variableName0Value) {
-  variableName0El.value = variableName0Value;
-}
-
-const variableQuery0Value = localStorage.getItem("variableQuery0");
-if (variableQuery0Value) {
-  variableQuery0El.value = variableQuery0Value;
-}
-
-const StateHandler = {
-  state: {
-    format: "",
-    variables: [
-      {
-        name: "",
-        query: "",
-      },
-    ],
-  },
-  hydrate: () => {
-    const state = JSON.parse(localStorage.getItem("state"));
-    if (state) {
-      StateHandler.state = state;
-
-      if (StateHandler.state.format !== "") {
-        // formatEl.value = StateHandler.state.format;
-      }
-
-      StateHandler.state.variables.forEach((variable, i) => {
-        createVariableHTML(i, variable);
-      });
-    }
-  },
-  setFormat: (value) => {
-    StateHandler.state.format = value;
-    localStorage.setItem("state", JSON.stringify(StateHandler.state));
-  },
-  setQuery: (id, value) => {
-    StateHandler.state.variables[id].query = value;
-    localStorage.setItem("state", JSON.stringify(StateHandler.state));
-  },
-  setName: (id, value) => {
-    StateHandler.state.variables[id].name = value;
-    localStorage.setItem("state", JSON.stringify(StateHandler.state));
-  },
-  addVariable: () => {
-    StateHandler.state.variables = StateHandler.state.variables.concat({
-      name: "",
-      query: "",
-    });
-    createVariableHTML(
-      StateHandler.state.variables.length,
-      StateHandler.state.variables[StateHandler.state.variables.length - 1]
-    );
-    localStorage.setItem("state", JSON.stringify(StateHandler.state));
-  },
-};
-
-StateHandler.hydrate();
