a6fd3edbecec66f3354ba365c2f39ffe825abb94
Yushi Canastra <ycanastra@gmail.com>
2021-03-13 20:24:52 -0700

    Make sign script work (maybe)

--- scripts/signAddon.js
+++ scripts/signAddon.js
@@ -1,35 +1,20 @@
-console.log(process.env.ADDONS_MOZILLA_ORG_JWT_ISSUER);
-console.log(process.env.ADDONS_MOZILLA_ORG_JWT_SECRET);
-
 const { exec } = require("child_process");
 
 const apiKey = process.env.ADDONS_MOZILLA_ORG_JWT_ISSUER;
 const apiSecret = process.env.ADDONS_MOZILLA_ORG_JWT_SECRET;
 
-//exec(
-//  `yarn web-ext sign --api-key=${apiKey} --api-secret=${apiSecret}`,
-//  (err, stdout, stderr) => {
-//    if (err) {
-//      //some err occurred
-//      console.error(err);
-//    } else {
-//      // the *entire* stdout and stderr (buffered)
-//      console.log(`stdout: ${stdout}`);
-//      console.log(`stderr: ${stderr}`);
-//    }
-//  }
-//);
-
-const { spawn } = require("child_process");
-const child = spawn("yarn web-ext sign", [
-  `--api-key=${apiKey}`,
-  `--api-secret=${apiSecret}`
-]);
+const child = exec(`yarn web-ext sign --api-key=${apiKey} --api-secret=${apiSecret}`);
 // use child.stdout.setEncoding('utf8'); if you want text chunks
 child.stdout.on("data", chunk => {
   // data from the standard output is here as buffers
   console.log(chunk);
 });
+
+child.stderr.on("data", chunk => {
+  // data from the standard error is here as buffers
+  console.log(chunk);
+});
+
 // since these are streams, you can pipe them elsewhere
 // child.stderr.pipe(dest);
 child.on("close", code => {
